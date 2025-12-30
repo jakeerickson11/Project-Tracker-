bd9170478434d34ec1f394157f2299edc7bca67d
Jake Erickson <jakeerickson11@gmail.com>
2025-12-29 22:34:56 -0500

    Update cache name to project-tracker-cache-v11

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "project-tracker-cache-v10";
+const CACHE_NAME = "project-tracker-cache-v11";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json"];
 
 self.addEventListener("install", (event) => {
